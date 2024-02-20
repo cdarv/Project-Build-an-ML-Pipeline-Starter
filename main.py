30298d7182ac0f3cb6686c2607f44f1571c7d6b2
cdarv <chazdarvish@gmail.com>
2024-02-20 10:12:56 -0500

    Prepare for 1.0.0 release, all tests ran and completed in the pipeline

--- main.py
+++ main.py
@@ -13,12 +13,8 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
+    "test_regression_model"
 ]
-
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
@@ -49,47 +45,79 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            # input sample, do some basic cleaning and output a clean sample
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                 "main",
+                 parameters={
+                     "input_artifact": "sample.csv:latest",
+                     "output_artifact": "clean_sample.csv",
+                     "output_type": "clean_sample",
+                     "output_description": "Data with outliers and null values removed",
+                     "min_price": config['etl']['min_price'],
+                     "max_price": config['etl']['max_price']
+                 },
+            )
             pass
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            # Checking clean data to make sure it fits within min and max price
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                 "main",
+                 parameters={
+                     "csv": "clean_sample.csv:latest",
+                     "ref": "clean_sample.csv:reference",
+                     "kl_threshold": config["data_check"]["kl_threshold"],
+                     "min_price": config['etl']['min_price'],
+                     "max_price": config['etl']['max_price']
+                 },
+            )
             pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                },
+            )
+            pass      
+        
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                 "main",
+                 parameters={
+                     "trainval_artifact": "trainval_data.csv:latest",
+                     "output_artifact": "random_forest_export",
+                     "rf_config": rf_config,
+                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                     "val_size": config["modeling"]["val_size"],
+                     "stratify_by": config["modeling"]["stratify_by"]
+                 },
+            )
             pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
+            # Testing the regression model againt the prod export
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
             pass
-
 
 if __name__ == "__main__":
     go()
